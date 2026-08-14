cea6c8a502848eba037a493970c350ec136566c5
vsivagami294-ops <vsivagami294@gmail.com>
2026-08-14 19:05:39 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,18 @@
+r<link rel="stylesheet"
+href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.12/typed.min.js"></script>
+<script>
+document.addEventListener("DOMContentLoaded", function () {
+new Typed(".typing", {
+strings: [
+"Web Designer",
+"Frontend Developer",                   
+"IT Enthusiast" ],
+typeSpeed: 100,
+backSpeed: 60,
+backDelay: 1500,
+loop: true
+});  });
+</script>
+
+                                                                      
